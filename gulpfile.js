6793da8221228ef58982bc97aca38ea6c341ec63
Aleksey Gunchin <a.s.gunchin@gmail.com>
2020-01-06 12:16:35 +0400

    add sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ var gulp = require('gulp');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 
 function css_style(done) {
     gulp.src('./scss/style.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass({
             errLogToConsole: true,
             outputStyle: 'compressed'
@@ -14,6 +16,7 @@ function css_style(done) {
             cascade: false
         }))
         .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./css/'))
     done()
 }
