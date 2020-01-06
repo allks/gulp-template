21a5212fc49031c890defd6a69c9ed50b1421111
Aleksey Gunchin <a.s.gunchin@gmail.com>
2020-01-06 13:07:02 +0400

    add browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
 
 function css_style(done) {
     gulp.src('./scss/**/*.scss')
@@ -18,11 +19,28 @@ function css_style(done) {
         .pipe(rename({suffix: '.min'}))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./css/'))
+        .pipe(browserSync.stream())
     done()
 }
 
-function watchSass() {
-    gulp.watch('./scss/**/*', css_style)
+function sync(done) {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        },
+        port: 3000
+    })
 }
 
-gulp.task('default', gulp.series(watchSass))
+function browserReload(done) {
+    browserSync.reload()
+}
+
+function watchFile() {
+    gulp.watch('./scss/**/*', css_style);
+    gulp.watch('./**/*.html', browserReload);
+    gulp.watch('./**/*.js', browserReload);
+    gulp.watch('./**/*.pug', browserReload);
+}
+
+gulp.task('default', gulp.parallel(watchFile, sync))
